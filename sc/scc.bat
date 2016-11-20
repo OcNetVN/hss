2ebf933f320bc146c002f0739fd94ef7e39d8ff8
occbuu <occbuu@gmail.com>
2016-11-20 14:53:35 +0700

    8th commit

---
+++ sc/scc.bat
@@ -0,0 +1,2 @@
+@echo off
+start /d "C:\Program Files\Internet Explorer" IEXPLORE.EXE http://localhost:899/hss/test/auto_insert_statement
---
+++ sc/script.bat
@@ -0,0 +1,2 @@
+@echo off
+start /d iexplore.exe http://localhost/hss/super_admin/home_controller/auto_insert_in_10th
